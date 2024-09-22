2fc86dd2fa518872bee3d8a119aac358a761dc2d
sarait2 <saraitorres999@gmail.com>
2024-09-21 20:52:59 -0400

    "Initialize inventory with product objects"

---
+++ order_management_system.js
@@ -0,0 +1,11 @@
+// TASK 1 - Create an Inventory Array of Product Objects
+const inventory = [ 
+    { name: "espresso", price: 3, quantity: 10 },
+    { name: "latte", price: 2, quantity:7 },
+    { name: "iced coffee", price: 2, quantity: 15},
+    { name: "frappuccino", price: 4, quantity:12},
+];
+//"Initialize inventory with product objects"
+
+
+

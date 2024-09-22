5db7451e9b347a8186c2d6f6ef39e8575f46d3b1
sarait2 <saraitorres999@gmail.com>
2024-09-22 13:01:39 -0400

    "Initialize orders array"

--- order_management_system.js
+++ order_management_system.js
@@ -7,5 +7,28 @@ const inventory = [
 ];
 //"Initialize inventory with product objects"
 
+// Task 2 - Create an Orders Array of Order Objects
+let orders = [];
+//Example Order
+order.push({
+    customer: "Spencer Reid"
+    items: [
+         {name: 'espresso', quantity: 3},
+           {name: "latte", quantity: 7},
+    ],
+    status: "pending",
+    });
+// Include the order to the orders array
+orders.push(order);
+console.log(orders);
+ //"Initialize orders array"
+
+
+
+//Task 3 - Create a Function to Place an Order
+
+
+
+
 
 
